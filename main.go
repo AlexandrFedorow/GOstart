298ca1ff0661e1828d5816e12380215f8b56700a
AlexandrFedorow <75899441+AlexandrFedorow@users.noreply.github.com>
2021-08-06 22:57:35 +0300

    init

--- main.go
+++ main.go
@@ -1,1 +1,44 @@
+package main
+
+import "fmt"
+
+
+func Draw(point []string){
+	var pole string
+	var n int16
+	n = -1
+	for i:=0; i<9; i++{
+		n += 1
+		if (i % 3 == 0) && (i!=0){
+			pole += "|\n"
+		}
+			pole += "|" + point[n]	
+	}
+
+	pole += "|"
+	fmt.Println(pole)
+}
+
+func main(){
+	points := []string{"1","2","3","4","5","6","7","8","9"}
+	
+	Draw(points)
+
+	var action int16
+	turn := 1
+
+	for i:=0; i<9; i++{
+		fmt.Scanln(&action)
+
+		if turn % 2 != 0{
+			points[action-1] = "X"
+			turn++
+		}else{
+			points[action-1] = "O"
+			turn++
+		}
+		Draw(points)
+	}
+}
+
 
